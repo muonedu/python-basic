97cf98b8f833d922a545654bbc5f47ed64db44b5
Vicky Dasta <7861544+vck@users.noreply.github.com>
2019-11-16 15:01:49 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+def sum_list(x):
+   sum = 0
+   for i in x:
+      sum += i
+   return sum
+
+def str_cvt_lower(s):
+	"""konversi besar->kecil"""
+	return s.lower()
+
+def str_cvt_upper(s):
+	"""konversi kecil->besar"""
+	return s.upper()
+
+
+
+
+
